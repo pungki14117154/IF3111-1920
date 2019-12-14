65908b7f99227a7ce3451427ce21919ded97cafb
Afifah Dwikirani <47997108+AfifahDk@users.noreply.github.com>
2019-12-14 14:40:58 +0700

    menambahkan nav

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,9 +9,9 @@
 <body>
 	<div class="navbar">
 <ul class="navbar">
-  <li class="navbar"><img src="asset/css/laporlogo.png" alt="simbol" style=" height: 60px; width: 60px;"></li>
-  <li class="navbar"><a class="navbar"class="active" title="home" href="<?= base_url();?>home/index"><i class="fa fa-home" aria-hidden="true" style=" height: 60px; width: 60px;"></i></a></li>
-  <li class="navbar"><a class="navbar"class="active" title="about" href="<?= base_url();?>About/index"><i class="fas fa-address-card" aria-hidden="true" style=" height: 60px; width: 60px;"></i></a></li>
-  <li class="navbar"><a class="navbar"class="active" title="lapor" href="<?= base_url();?>data/detail"><i class="far fa-comment-alt-lines"aria-hidden="true" style=" height: 60px; width: 60px;"></i></a></li>
+  <li class="navbar"><i class="far fa-comments" aria-hidden="true" style="font-size:80px; float:center; height: 70px; margin: 20px; margin-left: 5px; width: 70px;"></i></a></li>
+  <li class="navbar"><a class="navbar"class="active" title="home" href="<?= base_url();?>home/index"><i class="fa fa-home" aria-hidden="true" style=" margin: 20px; height: 60px; font-size:40px; width: 60px;"></i></a></li>
+  <li class="navbar"><a class="navbar"class="active" title="about" href="<?= base_url();?>About/index"><i class="fas fa-address-card" aria-hidden="true" style=" margin: 20px; font-size:40px; height: 60px; width: 60px;"></i></a></li>
+  <li class="navbar"><a class="navbar"class="active" title="lapor" href="<?= base_url();?>data/detail"><i class="fas fa-archive"style=" margin: 20px;height: 60px; font-size:40px; width: 60px;"></i></a></li>
 </ul>
 </div>
